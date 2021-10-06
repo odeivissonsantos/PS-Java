d22af57ebbf612443146293f3f7d132b1730c0a3
Deivisson Santos <deivissonsantos@hotmail.com>
2021-10-06 15:21:37 -0300

    implementado Usuario Rest com todos os seus métodos CRUD e endpoints

---
+++ src/main/java/br/com/supera/gamestore/rest/UsuarioRest.java
@@ -0,0 +1,58 @@
+package br.com.supera.gamestore.rest;
+
+import br.com.supera.gamestore.dtos.UsuarioDTO;
+import br.com.supera.gamestore.models.Usuario;
+import br.com.supera.gamestore.services.UsuarioService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api/v1/usuarios")
+@CrossOrigin("*")
+public class UsuarioRest {
+
+    private final UsuarioService usuarioService;
+
+    public UsuarioRest(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UsuarioDTO>> listarTodosUsuarios() {
+        List<Usuario> list = usuarioService.listarTodosUsuarios();
+        List<UsuarioDTO> listDTO = list.stream().map(obj -> new UsuarioDTO(obj)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDTO);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> buscarUsuarioPorId(@PathVariable Long id){
+        Usuario obj = usuarioService.buscarUsuarioPorId(id);
+        return ResponseEntity.ok().body(obj);
+    }
+
+    @PostMapping
+    public ResponseEntity<Usuario> criarUsuario(@RequestBody Usuario obj) {
+        Usuario newObj= usuarioService.criarUsuario(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(newObj);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Usuario> atualizarUsuario(@PathVariable Long id, @RequestBody Usuario obj){
+        Usuario newObj = usuarioService.atualizarUsuario(id, obj);
+        return ResponseEntity.ok().body(newObj);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarUsuario(@PathVariable Long id){
+        usuarioService.deletarUsuario(id);
+        return ResponseEntity.noContent().build();
+    }
+
+
+}
