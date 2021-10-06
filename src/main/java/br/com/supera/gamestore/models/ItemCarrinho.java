c97d6f5a2db8e319e621224ae0c4aafd3fd54a63
Deivisson Santos <deivissonsantos@hotmail.com>
2021-10-06 12:44:19 -0300

    implementado a classe de itens para o carrinho

---
+++ src/main/java/br/com/supera/gamestore/models/ItemCarrinho.java
@@ -0,0 +1,23 @@
+package br.com.supera.gamestore.models;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+public class ItemCarrinho implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "quantidade")
+    private Integer quantidade;
+
+    @Column(name = "preco")
+    private BigDecimal preco;
+}
