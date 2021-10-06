7c1d69aa0159bdbc6c59ea3490d51f957b2140bb
Deivisson Santos <deivissonsantos@hotmail.com>
2021-10-06 10:40:19 -0300

    inicializacao  do projeto

---
+++ src/test/java/br/com/supera/gamestore/DesafioJavaGameStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.supera.gamestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioJavaGameStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
